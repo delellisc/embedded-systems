3f8a7c154c81f95b7f98e971c2b9441805bfac48
delellisc <delellis021@gmail.com>
2025-10-21 08:44:42 -0300

    Showing sequence before asking for input.

--- genius-estrela/genius-estrela.ino
+++ genius-estrela/genius-estrela.ino
@@ -15,60 +15,21 @@ int btn_yel = 8;
 int btn_ble = 9;
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(led_red, OUTPUT);
-  pinMode(led_grn, OUTPUT);
-  pinMode(led_yel, OUTPUT);
-  pinMode(led_ble, OUTPUT);
-  pinMode(btn_red, INPUT);
-  pinMode(btn_grn, INPUT);
-  pinMode(btn_yel, INPUT);
-  pinMode(btn_ble, INPUT);
+    Serial.begin(9600);
+    pinMode(led_red, OUTPUT);
+    pinMode(led_grn, OUTPUT);
+    pinMode(led_yel, OUTPUT);
+    pinMode(led_ble, OUTPUT);
+    pinMode(btn_red, INPUT);
+    pinMode(btn_grn, INPUT);
+    pinMode(btn_yel, INPUT);
+    pinMode(btn_ble, INPUT);
 }
 
 void ger_seq(){
-  for (int i = 0; i < seq_size; i++){
-    cor[i] = random(2,6);
-  }
-}
-
-/*
-
-void blink(int tempo){
-  millisATUAL = millis();
-  Serial.println(millisATUAL);
-  if(millisATUAL - millisANT >= tempo){
-    estado_LED = !estado_LED;
-    digitalWrite(pin_LED, estado_LED);
-    millisANT = millisATUAL;
-  }
-}
-
-*/
-
-/*
-void read_btn(int btn_pin, int tempo){
-  millisATUAL = millis();
-   if(millisATUAL - millisANT >= tempo){
-     if (digitalRead(btn_pin)){
-     	return 1;
-     }
-     millisANT = millisATUAL;
-  }
-}
-*/
-
-int read_btn(int btn_pin, int tempo){
-  millisATUAL = millis();
-  while(millisATUAL - millisANT >= tempo){
-    millisATUAL = millis();
-    if (digitalRead(btn_pin)){
-      millisANT = millisATUAL;
-      return 1;
+    for (int i = 0; i < seq_size; i++){
+        cor[i] = random(2,6);
     }
-  }
-  millisANT = millisATUAL;
-  return 0;
 }
 
 void read_btn_red(){
@@ -116,43 +77,52 @@ void read_btn_ble(){
 }
 
 void print_seq(int fim){
-  for (int i = 0; i < fim; i++){
-    Serial.print(i);
-    Serial.print(": ");
-    int valor = cor[i];
-    switch (valor) {
-      case 2:
-        read_btn_red();
-        break;
-      case 3:
-        read_btn_grn();
-        break;
-      case 4:
-        read_btn_yel();
-        break;
-      case 5:
-        read_btn_ble();
-        break;
-      default:
-        break;
+    for (int i = 0; i < fim; i++){
+        Serial.print(i);
+        Serial.print(": ");
+        int valor = cor[i];
+        digitalWrite(valor, HIGH);
+        Serial.print(cor[i]);
+      	Serial.print("\n");
+        delay(100);
+        digitalWrite(valor, LOW);
     }
-    digitalWrite(valor, HIGH);
-    //Serial.print(cor[i]);
-    delay(500);
-    digitalWrite(valor, LOW);
-    Serial.print("\n");
-  }
+    for (int i = 0; i < fim; i++){
+        Serial.print(i);
+        Serial.print(": ");
+        int valor = cor[i];
+        switch (valor) {
+          case 2:
+            read_btn_red();
+            break;
+          case 3:
+            read_btn_grn();
+            break;
+          case 4:
+            read_btn_yel();
+            break;
+          case 5:
+            read_btn_ble();
+            break;
+          default:
+            break;
+        }
+        digitalWrite(valor, HIGH);
+        delay(200);
+        digitalWrite(valor, LOW);
+        Serial.print("\n");
+    }
 }
 
 int i = 1;
 
 void loop(){ 
-  Serial.print("\nIteration n: ");
-  Serial.print(i);
-  Serial.print("\n");
-  ger_seq();
-  for (int j = 0; j < seq_size + 1; j++){
-    print_seq(j);
-  }
-  i++;
+    Serial.print("\nIteration n: ");
+    Serial.print(i);
+    Serial.print("\n");
+    ger_seq();
+    for (int j = 0; j < seq_size + 1; j++){
+        print_seq(j);
+    }
+    i++;
 }
